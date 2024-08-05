d53849c2f4260a6ba04851aa72fc5b912b9eadf4
Artem <artembabichev@gmail.com>
2024-08-05 20:51:12 +0300

    api login connect3

--- app/Http/Controllers/TelegramAuthController.php
+++ app/Http/Controllers/TelegramAuthController.php
@@ -22,8 +22,8 @@ class TelegramAuthController extends Controller
         }
 
         // Декодируем JSON строку user
-        $userData = json_decode($data['user'], true);
-        Log::info('Decoded user data: ', $userData);
+        $data['user'] = json_decode($data['user'], true);
+        Log::info('Decoded user data: ', $data['user']);
 
         if (json_last_error() !== JSON_ERROR_NONE) {
             Log::error('Invalid user data', ['error' => json_last_error_msg()]);
@@ -31,11 +31,11 @@ class TelegramAuthController extends Controller
         }
 
         // Формируем строку для проверки
-        $dataCheckString = $this->generateDataCheckString($data['auth_date'], $userData);
+        $dataCheckString = $this->generateDataCheckString($data);
         Log::info('Data Check String: ' . $dataCheckString);
 
         // Валидация хэша
-        if ($this->validateTelegramData($dataCheckString, $data['hash'], $botToken)) {
+        if ($this->validateTelegramData($data, $dataCheckString, $botToken)) {
             // Валидация успешна
             Log::info('Validation successful');
             return response()->json([
@@ -50,34 +50,35 @@ class TelegramAuthController extends Controller
         }
     }
 
-    private function generateDataCheckString($authDate, $userData)
+    private function generateDataCheckString($data)
     {
         $dataCheckString = [];
 
-        // Добавляем auth_date отдельно
-        $dataCheckString[] = 'auth_date=' . $authDate;
-
-        // Добавляем параметры из user
-        foreach ($userData as $key => $value) {
-            if ($value === null) {
-                $value = '';
+        foreach ($data as $key => $value) {
+            if ($key !== 'hash') {
+                if (is_array($value)) {
+                    foreach ($value as $subKey => $subValue) {
+                        $dataCheckString[] = $subKey . '=' . $subValue;
+                    }
+                } else {
+                    $dataCheckString[] = $key . '=' . $value;
+                }
             }
-            $dataCheckString[] = $key . '=' . $value;
         }
 
-        // Сортируем строку
         sort($dataCheckString);
         return implode("\n", $dataCheckString);
     }
 
-    private function validateTelegramData($dataCheckString, $checkHash, $botToken)
+    private function validateTelegramData($data, $dataCheckString, $botToken)
     {
+        $checkHash = $data['hash'];
+
         // Создаем секретный ключ
         $secretKey = hash_hmac('sha256', $botToken, 'WebAppData', true);
         $calculatedHash = hash_hmac('sha256', $dataCheckString, $secretKey);
 
         // Логируем строку для проверки и рассчитанный хэш
-        Log::info('Data Check String: ' . $dataCheckString);
         Log::info('Secret Key (hex): ' . bin2hex($secretKey));
         Log::info('Calculated Hash: ' . $calculatedHash);
         Log::info('Received Hash: ' . $checkHash);
