d8c6d8b3f250e31b754ae0f6eed320b1a52a28a4
Artem <artembabichev@gmail.com>
2024-08-06 01:42:31 +0300

    api login connect3

--- app/Http/Controllers/TelegramAuthController.php
+++ app/Http/Controllers/TelegramAuthController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\PersonalAccessToken;
 
@@ -14,14 +14,14 @@ class TelegramAuthController extends Controller
     public function authenticate(Request $request)
     {
         // Логирование всего запроса
-        //Log::info('Received request', $request->all());
+        Log::info('Received request', $request->all());
 
         // Получение данных из запроса
         $initData = $request->input('initData');
 
         // Парсинг данных
         parse_str($initData, $data);
-        //Log::info('Parsed init data', $data);
+        Log::info('Parsed init data', $data);
 
         // Получение хеша из данных
         $receivedHash = $data['hash'] ?? null;
@@ -38,36 +38,43 @@ class TelegramAuthController extends Controller
 
         // Создание строки для проверки
         $dataCheckString = urldecode(http_build_query($data, '', "\n"));
-        //Log::info('Data check string', ['data_check_string' => $dataCheckString]);
+        Log::info('Data check string', ['data_check_string' => $dataCheckString]);
 
         // Получение ключа бота из окружения
         $botToken = env('TELEGRAM_BOT_TOKEN');
 
         // Создание секретного ключа
         $secretKey = hash_hmac('sha256', $botToken, 'WebAppData', true);
-        //Log::info('Secret key generated', ['secret_key' => bin2hex($secretKey)]);
+        Log::info('Secret key generated', ['secret_key' => bin2hex($secretKey)]);
 
         // Создание хеша для проверки
         $computedHash = hash_hmac('sha256', $dataCheckString, $secretKey);
-        //Log::info('Computed hash', ['computed_hash' => $computedHash]);
+        Log::info('Computed hash', ['computed_hash' => $computedHash]);
 
         // Проверка хеша
         if (hash_equals($computedHash, $receivedHash)) {
             Log::info('Hash verification passed');
 
+            // Декодируем поле user из JSON
+            $userData = json_decode($data['user'], true);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                Log::error('Error decoding user data', ['error' => json_last_error_msg()]);
+                return response()->json(['error' => 'Error decoding user data'], 400);
+            }
+
             // Проверка наличия пользователя по telegram_id
-            $telegramId = $data['user']['id'];
+            $telegramId = $userData['id'];
             $user = User::where('telegram_id', $telegramId)->first();
 
             if (!$user) {
                 // Если пользователя нет, зарегистрируем его
                 $user = User::create([
                     'telegram_id' => $telegramId,
-                    'first_name' => $data['user']['first_name'],
-                    'last_name' => $data['user']['last_name'] ?? null,
-                    'username' => $data['user']['username'],
-                    'language_code' => $data['user']['language_code'] ?? null,
-                    'allows_write_to_pm' => $data['user']['allows_write_to_pm'] ?? false,
+                    'first_name' => $userData['first_name'],
+                    'last_name' => $userData['last_name'] ?? null,
+                    'username' => $userData['username'],
+                    'language_code' => $userData['language_code'] ?? null,
+                    'allows_write_to_pm' => $userData['allows_write_to_pm'] ?? false,
                 ]);
                 Log::info('User registered', ['user' => $user]);
             }
