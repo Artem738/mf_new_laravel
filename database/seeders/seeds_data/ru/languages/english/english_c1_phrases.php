d3029c0359c065de4fbd3478f68006719493bbed
Artem <artembabichev@gmail.com>
2026-07-04 18:09:38 +0300

    c1+c2 update

--- database/seeders/seeds_data/ru/languages/english/english_c1_phrases.php
+++ database/seeders/seeds_data/ru/languages/english/english_c1_phrases.php
@@ -4,57 +4,70 @@ return [
     'name' => '🟣 🇬🇧 Английский: C1 Фразы и клише',
     'description' => 'Продвинутые разговорные фразы и академические клише для эссе, дебатов и публичных выступлений (уровень Advanced).',
     'cards' => [
-        // --- Introducing a Topic / Opening ---
         ['q' => 'Широко признано, что...', 'a' => 'It is widely acknowledged that... / It is generally accepted that...'],
         ['q' => 'Часто утверждают, что...', 'a' => 'It is often claimed that...'],
         ['q' => 'Тема (чего-то) вызывает жаркие споры', 'a' => 'The issue of... is a highly debated topic'],
         ['q' => 'Начнем с того, что...', 'a' => 'To begin with, ... / First and foremost, ...'],
         ['q' => 'В современном мире...', 'a' => 'In today\'s fast-paced world, ... / In contemporary society, ...'],
-
-        // --- Expressing Strong Opinions ---
         ['q' => 'Я твердо убежден, что...', 'a' => 'I firmly believe that... / It is my firm conviction that...'],
         ['q' => 'Нет никаких сомнений в том, что...', 'a' => 'There is no shadow of a doubt that...'],
         ['q' => 'Мне кажется совершенно очевидным, что...', 'a' => 'It seems abundantly clear to me that...'],
         ['q' => 'Я склоняюсь к мнению, что...', 'a' => 'I am inclined to believe that...'],
-
-        // --- Adding Arguments ---
         ['q' => 'Мало того / Более того', 'a' => 'Moreover / Furthermore / What is more'],
         ['q' => 'В дополнение к этому...', 'a' => 'In addition to this, ...'],
         ['q' => 'Еще один момент, который следует учитывать...', 'a' => 'Another point worth considering is...'],
         ['q' => 'Не говоря уже о...', 'a' => 'Not to mention...'],
-
-        // --- Providing Examples ---
         ['q' => 'Ярким примером этого является...', 'a' => 'A prime example of this would be... / A glaring example is...'],
         ['q' => 'А именно (в частности)...', 'a' => 'Namely...'],
         ['q' => 'Возьмем, к примеру...', 'a' => 'Take... for instance.'],
         ['q' => 'Чтобы проиллюстрировать эту мысль...', 'a' => 'To illustrate this point, ...'],
-
-        // --- Contrasting / Providing Counterarguments ---
         ['q' => 'Несмотря на это...', 'a' => 'Having said that, ... / That being said, ...'],
         ['q' => 'В отличие от (чего-то)', 'a' => 'In stark contrast to... / As opposed to...'],
         ['q' => 'Противники этой идеи утверждают, что...', 'a' => 'Opponents of this idea claim that...'],
         ['q' => 'Однако нельзя игнорировать тот факт, что...', 'a' => 'However, one cannot ignore the fact that...'],
-
-        // --- Cause and Effect ---
         ['q' => 'По этой причине (в результате)', 'a' => 'For this reason / Consequently'],
         ['q' => 'Это, в свою очередь, приводит к...', 'a' => 'This, in turn, leads to... / This gives rise to...'],
         ['q' => 'Проистекает из / Является результатом', 'a' => 'Stems from / Results from'],
-
-        // --- Emphasizing ---
         ['q' => 'Следует подчеркнуть, что...', 'a' => 'It should be emphasized that...'],
         ['q' => 'Жизненно важно (критически важно)', 'a' => 'It is of paramount importance... / It is crucial...'],
         ['q' => 'В первую очередь...', 'a' => 'First and foremost, ...'],
-
-        // --- Summarizing / Concluding ---
         ['q' => 'Подводя итог...', 'a' => 'To sum up, ... / In conclusion, ...'],
         ['q' => 'Учитывая все обстоятельства...', 'a' => 'Taking everything into consideration, ... / All things considered, ...'],
         ['q' => 'Взвесив все за и против...', 'a' => 'Weighing up the pros and cons, ...'],
         ['q' => 'В конечном счете...', 'a' => 'Ultimately, ... / At the end of the day, ...'],
-
-        // --- Conversational Advanced Phrases ---
         ['q' => 'Давайте проясним одну вещь...', 'a' => 'Let\'s get one thing straight...'],
         ['q' => 'Если дойдет до крайности (в худшем случае)', 'a' => 'If push comes to shove...'],
         ['q' => 'Это за гранью моего понимания', 'a' => 'It\'s beyond my comprehension'],
         ['q' => 'Не хочу показаться грубым, но...', 'a' => 'With all due respect...'],
+        ['q' => 'Само собой разумеется', 'a' => 'It goes without saying'],
+        ['q' => 'В конечном счете', 'a' => 'Ultimately / In the final analysis'],
+        ['q' => 'Проще говоря', 'a' => 'Simply put / To put it simply'],
+        ['q' => 'С одной стороны... С другой стороны...', 'a' => 'On the one hand... On the other hand...'],
+        ['q' => 'Короче говоря', 'a' => 'To cut a long story short / Long story short'],
+        ['q' => 'Как правило', 'a' => 'As a rule of thumb / As a general rule'],
+        ['q' => 'В какой-то степени', 'a' => 'To some extent / To a certain degree'],
+        ['q' => 'В значительной степени', 'a' => 'By and large / For the most part'],
+        ['q' => 'Не говоря уже о...', 'a' => 'Not to mention... / Let alone...'],
+        ['q' => 'Помимо прочего', 'a' => 'Among other things'],
+        ['q' => 'Прежде всего (Во-первых)', 'a' => 'First and foremost'],
+        ['q' => 'В довершение всего', 'a' => 'To top it all off / To crown it all'],
+        ['q' => 'В свете этого', 'a' => 'In light of this'],
+        ['q' => 'С учетом обстоятельств', 'a' => 'Under the circumstances'],
+        ['q' => 'Насколько мне известно', 'a' => 'To the best of my knowledge / As far as I know'],
+        ['q' => 'Честно говоря', 'a' => 'To be perfectly honest / Frankly speaking'],
+        ['q' => 'Это вопрос времени', 'a' => 'It is a matter of time'],
+        ['q' => 'Тут есть загвоздка', 'a' => 'There is a catch'],
+        ['q' => 'Суть в том, что...', 'a' => 'The bottom line is that...'],
+        ['q' => 'Помимо всего прочего', 'a' => 'Apart from anything else'],
+        ['q' => 'Как выясняется / В итоге', 'a' => 'As it turns out'],
+        ['q' => 'Иметь в виду (Учитывать)', 'a' => 'To bear in mind / To keep in mind'],
+        ['q' => 'Без тени сомнения', 'a' => 'Without a shadow of a doubt'],
+        ['q' => 'Если дойдет до крайностей', 'a' => 'If push comes to shove'],
+        ['q' => 'На первый взгляд', 'a' => 'At first glance / On the face of it'],
+        ['q' => 'В долгосрочной перспективе', 'a' => 'In the long run'],
+        ['q' => 'С другой стороны (Наоборот)', 'a' => 'Conversely / Vice versa'],
+        ['q' => 'По этой причине', 'a' => 'For this very reason'],
+        ['q' => 'Во всех смыслах (Фактически)', 'a' => 'To all intents and purposes'],
+        ['q' => 'Дело в том, что...', 'a' => 'The point is that...'],
     ],
 ];
